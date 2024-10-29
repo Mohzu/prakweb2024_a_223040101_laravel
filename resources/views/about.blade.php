7dad0fdaec07e4c5f91fd679e7cc5c86121223ec
Mohzu <mochzuhdi04@gmail.com>
2024-10-29 16:08:27 +0700

    Video 4 Blade Templating Engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,8 +7,10 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<a href="/">Home</a>
-<a href="/about">About</a>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
     <h1>Halaman About</h1>
     <h3>Owner: {{ $nama }}</h3>
     <img src="img/zuhdi.jpg" alt="" width="200">
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Contact</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+            <a href="https://instagram.com/m.zuhdimaulana">Instagram</a>
+        </li>
+        <li>
+            <a href="https://x.com/m.zuhdimaulana">X</a>
+        </li>
+        <li>
+            <a href="https://youtube.com/m.zuhdimaulana">Youtube</a>
+        </li>
+    </ul>
+</body>
+</html>
