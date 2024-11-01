09cec0373f1c7aad60a08a63d45dcea6db06ae36
Mohzu <mochzuhdi04@gmail.com>
2024-11-02 00:54:09 +0700

    Video 9 Eloquent ORM dan Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,41 +1,12 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post {
-    public static function all() 
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-               'title' => 'Judul Artikel 1',
-               'author' => 'Moch Zuhdi',
-               'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab porro asperiores ut ex, in tenetur. Beatae quia omnis nihil esse voluptates libero, non iusto explicabo provident quidem atque fuga. Sit?' 
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Moch Zuhdi',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui quisquam similique quo unde illo, mollitia odio architecto enim est quam earum labore ratione numquam quae, itaque repudiandae nam reprehenderit soluta?' 
-             ]
-            ];
-    }
-
-    public static function find($slug): array 
-    {
-
-
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-
-        if(! $post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
-
