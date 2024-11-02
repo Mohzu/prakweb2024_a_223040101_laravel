9190311cce08d9871c931d7b7f608bbf9273e7ec
Mohzu <mochzuhdi04@gmail.com>
2024-11-03 04:07:23 +0700

    Video 12 Post Category

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,35 +10,20 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
+        'username', // Ditambahkan
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -50,4 +35,5 @@ class User extends Authenticatable
     public function posts(): HasMany {
        return $this->hasMany(Post::class, 'author_id');
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Route;
 
 
@@ -26,11 +28,14 @@ Route::get('/posts/{post:slug}', function(Post $post) {
 
 });
 
-Route::get('/authors/{user}', function(User $user) {
-    
-    return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts ]);
-
+Route::get('/authors/{user:username}', function(User $user) {
+    return view('posts', ['title' =>count($user->posts) . 'Articles by ' . $user->name, 'posts' => $user->posts]);
 });
+
+Route::get('/categories/{category:slug}', function(Category $category) {
+    return view('posts', ['title' => 'Articles in:'  . $category->name, 'posts' => $category->posts]);
+});
+
 
 Route::get('/contact', function () {
     return view('contact', ['title'=> 'Contact']);
