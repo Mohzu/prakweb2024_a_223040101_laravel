4b51d62f41a44da394b10404337031c70458ea56
Mohzu <mochzuhdi04@gmail.com>
2024-11-02 21:56:20 +0700

    Video 11 Eloquent Relationship

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -21,6 +23,12 @@ Route::get('/posts/{post:slug}', function(Post $post) {
     
     
     return view('post', ['title' => 'Single Post', 'post' => $post  ]);
+
+});
+
+Route::get('/authors/{user}', function(User $user) {
+    
+    return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts ]);
 
 });
 
